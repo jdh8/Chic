5931a2ff033944b7749474707763daa856c76e16
Chen-Pang He <chen.pang.he@jdh8.org>
2016-10-22 23:46:53 +0800

    Refactor insertion of unary expressions

--- Dictionary.hpp
+++ Dictionary.hpp
@@ -35,11 +35,9 @@ class Dictionary
     std::vector<std::vector<Number>> _hierarchy;
     const int _digit;
 
-    void emplace(const Number&, const Expression<Number>&);
+    void emplace(Number, Expression<Number>);
     void binary(const Number&, const Number&);
-
-    template<typename Function>
-    void unary(const Function&, char, const Number&);
+    void unary();
 
   public:
     Dictionary(int);
@@ -86,10 +84,14 @@ int Dictionary<Number>::digit() const
 }
 
 template<typename Number>
-void Dictionary<Number>::emplace(const Number& key, const Expression<Number>& expr)
+void Dictionary<Number>::emplace(Number key, Expression<Number> expr)
 {
-  if (key && _graph.emplace(key, expr).second)
+  while (key && _graph.emplace(key, expr).second) {
     _hierarchy.back().emplace_back(key);
+
+    expr = { key, 2 };
+    key = sqrt(key);
+  }
 }
 
 template<typename Number>
@@ -107,44 +109,41 @@ void Dictionary<Number>::binary(const Number& x, const Number& y)
 }
 
 template<typename Number>
-template<typename Function>
-void Dictionary<Number>::unary(const Function& function, char symbol, const Number& thresh)
+void Dictionary<Number>::unary()
 {
   auto& destination = _hierarchy.back();
-  std::vector<Number> source;
+  std::size_t length = destination.size();
 
-  for (auto x: destination)
-    for (auto y = function(x); y > thresh; y = function(y))
-      if (_graph.emplace(y, Expression<Number>(x, symbol)).second)
-        source.emplace_back(x = y);
+  for (std::size_t k = 0; k < length; ++k) {
+    auto x = destination[k];
+    auto y = factorial(x);
 
-  destination.insert(destination.end(), source.begin(), source.end());
+    // Due to Bertrand's postulate, from 2! on, factorials cannot be a perfect square.
+    while (y > 2 && _graph.emplace(y, Expression<Number>(x, '!')).second) {
+      destination.emplace_back(y);
+      x = y;
+      y = factorial(y);
+    }
+  }
 }
 
 template<typename Number>
 void Dictionary<Number>::grow()
 {
-  typedef Number Function(const Number&);
-
-  const Function* psqrt = sqrt;
-  const Function* pfact = factorial;
-
   _hierarchy.emplace_back();
 
   const auto* base = _hierarchy.data() - 1;
   auto size = _hierarchy.size();
   Number root(size, _digit);
 
-  _graph[root] = root;
-  _hierarchy.back().push_back(root);
+  emplace(root, root);
 
   for (auto length = size / 2; length > 0; --length)
     for (const auto& x: base[length])
       for (const auto& y: base[size - length])
         binary(x, y);
 
-  unary(psqrt, Expression<Number>::sqrt, 1);
-  unary(pfact, '!', 2);
+  unary();
 }
 
 template<typename Number>
